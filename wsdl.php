826df3d8b1e03c8e3da4fce4ec37e97b9e1f527e
yuanxing008 <yuanxing008@xtu.edu.cn>
2015-04-09 00:39:58 +0800

    Code Upload
    That's okay. Maybe it will have some BUGS, Welcome to report for me.

---
+++ wsdl.php
@@ -0,0 +1,12 @@
+<?php 
+	/*
+	*	@Author yuanxing008
+	*	@date   2015-04-09
+	*	@VerSion 0.1
+	*
+   	*/
+	header('content-type:text/html;charset=utf-8');
+	include 'class.php';
+	$param  = array('Username'=>'xxx','Password'=>'xxx','authentication' => SOAP_AUTHENTICATION_BASIC);
+	$s = new SoapClient('http://xxx/xxx?wsdl',$param);
+?>
